a26391b007a836f3ef0d01a4063e0e4a05d33406
David Bach <david.bach21@gmail.com>
2018-05-27 19:08:38 +0200

    Add test_arguments method and help string

--- daily-check.py
+++ daily-check.py
@@ -1,14 +1,26 @@
-NOTE_PATH='/Users/david/ownCloud/nvALT/Taskpaperx.txt'
+import sys
 
-# How to run:
-# task_keeper:
-#   -h(--help): Return this string
-#   -w: Weekly schedule and test of the task file
-#   -m: Monthly schedule and testing of task file
-#   -n: Not changeing the task file
-#
+NOTE_PATH='/Users/david/ownCloud/nvALT/Taskpaperx.txt'
+TASK = ''
+
+HELP_STRING= """ task_keeper:
+        -h(--help): Return this string
+        -w: Weekly schedule and test of the task file
+        -m: Monthly schedule and testing of task file
+        -n: Not changeing the task file"""
 
 def test_arguments():
+    if len(sys.argv) != 2:
+        print(HELP_STRING)
+        exit(1)
+    elif sys.argv[1] == "-h":
+        print(HELP_STRING)
+        exit(1)
+    elif sys.argv[1] in ["-w", "-m"]:
+        pass
+    else:
+        print(HELP_STRING)
+        exit(1)
 
 
 def create_notification(content):
@@ -29,10 +41,11 @@ def main():
         for line in f.readlines():
             splitted_line = [tag.rstrip() for tag in line.split('@')]
             tags = splitted_line[1:]
-            content = splitted_line[0].split('-')[1].strip()
             if tags:
+                content = splitted_line[0].split('-')[1].strip()
                 print(tags)
                 print(content)
 
 if __name__ == "__main__":
+    test_arguments()
     main()
